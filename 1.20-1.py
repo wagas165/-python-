6255cdd716b6804235b2668dd8bdb7f6be0b209c
Zhang <3162635401@qq.com>
2022-01-21 10:50:36 +0800

    新的开始！

---
+++ 1.20-1.py
@@ -0,0 +1,15 @@
+a='hello world!'
+answer=[]
+answer.append('1.操作系统使得各硬件可以有机结合进行工作，软件以操作系统为定义域，特定目的群为值域的映射')
+answer.append('2.windows、linux、macos')
+answer.append('3.作用是翻译编程语言代码使得电脑可执行，编译器用来翻译project为临时文件再执行之，解释器对代码边解释边执行')
+answer.append('4.分为编译型和解释型；高级与低级编程语言')
+answer.append('5.Cpython、jython...')
+answer.append('6.有2.x，3.x版本，现在是3.9.0')
+answer.append('7.可以使得系统在运行某一已有目录下的文件时无需再写前缀')
+answer.append('8.python是解释器，pycharm是IDE等价类中的一个代表元')
+print(a)
+for i in range(8):
+    print(answer[i])
+
+
