d2d5b148e88197a2318b5b6fde1b25930155c025
yichiz <50203296+wagas165@users.noreply.github.com>
2025-09-26 16:12:58 -0400

    Use canonical keys during training

--- ultimate_ttt/train.py
+++ ultimate_ttt/train.py
@@ -16,7 +16,7 @@ def play_episode(agent: UltimateTTTRLAI, epsilon: float) -> Tuple[str, int]:
     move_count = 0
 
     while not game.terminal:
-        state_key = game.serialize(current_player)
+        state_key = agent._state_key(game, current_player)
         moves = game.available_moves()
         move = agent.choose_action(state_key, moves, epsilon)
         game.make_move(current_player, move)
@@ -37,7 +37,7 @@ def play_episode(agent: UltimateTTTRLAI, epsilon: float) -> Tuple[str, int]:
             )
 
         next_player = "O" if current_player == "X" else "X"
-        next_state_key = game.serialize(next_player)
+        next_state_key = agent._state_key(game, next_player)
         next_moves = game.available_moves()
         agent.update(state_key, move, 0.0, next_state_key, next_moves)
         current_player = next_player
